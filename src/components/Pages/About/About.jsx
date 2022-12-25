bc2db3f22b6a9c6d12b30fa01f46016dda173493
joerdixon <joseph35dixon@gmail.com>
2022-12-25 13:39:47 -0800

    about section tailwind done

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import './About.css'
 import headshot from '../../../assets/niceheadshot.jpg'
 
-// TODO: Transplant Tailwind
 // TODO: Add animations on screen load, this is the landing page.
 
 function About(props) {
     return (
-        <div className='about-container'>
-            <div className='img-container'>
-                <img src={headshot} alt="Me!" />
+        <div className='h-2/3 flex justify-evenly items-center'>
+            <div className='w-1/4 flex justify-around'>
+                <img className='w-full rounded-full my-0 mx-auto' src={headshot} alt="Me!" />
             </div>
-            <div className='bio-container'>
-                <h1>About Me</h1>
-                <p>Welcome to my portfolio! My name is Joe and I am a web enthusiest currently working towards becoming a Full Stack Web Developer. Since I was young, I've always loved computers and the internet so I'm trying to make it my job! I live in Seattle, WA.</p>
+            <div className='w-1/2'>
+                <h1 className='w-1/2 text-center mx-auto my-0'>About Me</h1>
+                <p className='m-3 text-center w-full'>Welcome to my portfolio! My name is Joe and I am a web enthusiest currently working towards becoming a Full Stack Web Developer. Since I was young, I've always loved computers and the internet so I'm trying to make it my job! I live in Seattle, WA.</p>
             </div>
         </div>
     )
