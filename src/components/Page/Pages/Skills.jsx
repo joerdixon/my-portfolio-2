9e14e4d3b6782006bdc70a3ea6357bee283ca4a8
joerdixon <joseph35dixon@gmail.com>
2022-12-28 15:03:20 -0800

    skills section reconsidered

--- src/components/Page/Pages/Skills.jsx
+++ src/components/Page/Pages/Skills.jsx
@@ -5,39 +5,77 @@ import resume from '../../../assets/resume.pdf'
 
 function Skills(props) {
     return (
-        <div className='w-5/6'>
+        <div className='w-5/6 flex flex-col justify-top items-center'>
             <h2 className='sectionheading'>Skills</h2>
             <div className='w-full flex justify-evenly flex-wrap'>
+                <div>
+                {/* Front End Skills */}
+                <h2 className='ss-head'>Front End</h2>
                 <div className='skillsection'>
-                    <h2>Front End</h2>
-                    <ul>
-                        <li>HTML5</li>
-                        <li>CSS</li>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Tailwind</li>
-                    </ul>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>HTML</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>CSS</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>JavaScript</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>React</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>Tailwind</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>Bootstrap</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>Bulma</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>jQuery</p>
+                    </div>
                 </div>
-                <div className='skillsection'>
-                    <h2>Back End</h2>
-                    <ul>
-                        <li>Node.js</li>
-                        <li>MySQL</li>
-                        <li>MongoDB</li>
-                        <li>GraphQL</li>
-                    </ul>
                 </div>
+                {/* Back End Skills */}
+                <h2 className='ss-head'>Back End</h2>
                 <div className='skillsection'>
-                    <h2>Concepts</h2>
-                    <ul>
-                        <li>Object-Oriented Programming</li>
-                        <li>MVC</li>
-                        <li>Data Structures</li>
-                        <li>Algorithms</li>
-                    </ul>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>Express.js</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>Back end skill</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>Back end skill</p>
+                    </div>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>Back end skill</p>
+                    </div>
+                </div>
+                {/* Tools */}
+                <h2 className='ss-head'>Tools</h2>
+                <div className='skillsection'>
+                    <div className='skill'>
+                        <img className='skill-icon' src="https://placeholder.pics/svg/40" alt="" />
+                        <p className='skill-name'>Concept</p>
+                    </div>
                 </div>
             </div>
-            <div className=''>
+            <div className='border rounded bg-alt-bg mt-16'>
                 <a href={resume} download='Resume.pdf'>Download Resume</a>
             </div>
         </div>
