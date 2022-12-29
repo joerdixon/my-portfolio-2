02592a9f235496d73f463e8ae659e24d20a9aac0
joerdixon <joseph35dixon@gmail.com>
2022-12-28 17:31:22 -0800

    commit before build

--- src/components/Page/Pages/About.jsx
+++ src/components/Page/Pages/About.jsx
@@ -9,12 +9,11 @@ function About(props) {
     return (
         <div className='flex flex-col justify-top items-center w-5/6'>
             <h2 className='sectionheading'>About Me</h2>
-            <div className='w-1/4 flex justify-around'>
-                <img className='w-full rounded-full my-0 mx-auto' src={headshot} alt="Picture of Joe Dixon" />
+            <div className='w-1/4 flex justify-around mt-10'>
+                <img className='w-full rounded-full my-0 mx-auto' src={headshot} alt="Joe Dixon" />
             </div>
-            <div className='w-1/2'>
-                <h1 className='w-1/2 text-center mx-auto my-0'>About Me</h1>
-                <p className='m-3 text-center w-full'>Welcome to my portfolio! My name is Joe and I am a web enthusiest currently working towards becoming a Full Stack Web Developer. Since I was young, I've always loved computers and the internet so I'm trying to make it my job! I live in Seattle, WA.</p>
+            <div className='w-1/2 mt-20'>
+                <p className='m-3 text-center w-full'>Welcome to my portfolio!<br></br><br></br>My name is Joe and I am a web enthusiest currently working towards becoming a Full Stack Web Developer. Since I was young, I've always loved computers and the internet so I'm trying to make it my job! I live in Seattle, WA.</p>
             </div>
         </div>
     )
