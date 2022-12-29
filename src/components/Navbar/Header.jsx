62fc27f3d83dd702f0cfc5473cfdd9219c9b0fcd
joerdixon <joseph35dixon@gmail.com>
2022-12-28 21:02:47 -0800

    started responsivness

--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -10,18 +10,18 @@ import gmsvg from '../../assets/icons/mail.svg'
 
 function Header(props) {
     return (
-        <div className='flex flex-col justify-between w-1/6 border-r pt-10 pb-5'>
+        <div className='flex w-full md:flex-col justify-between md:w-1/6 md:border-r pt-10 pb-5'>
             {/* Nameplate*/}
-            <div className="flex justify-evenly items-center mb-10">
-                <img className="rounded-full w-1/6" src={headshot} alt="Joe Dixon" />
+            <div className="flex justify-evenly items-center mb-10 w-full">
+                <img className="rounded-full w-1/6 hidden md:block" src={headshot} alt="Joe Dixon" />
                 <h1 className='text-center text-xl font-semibold tracking-wide'>Joe Dixon</h1>
             </div>
             {/* Navbar */}
             <Navigation view={props.view} setView={props.setView} />
             {/* Resume */}
-            <a className="bg-highlight text-black p-3 font-bold rounded w-2/3 mx-auto my-0 text-center mt-20 hover:resume-hover" href={resume} download="joes-resume">Download Resume</a>
+            <a className="bg-highlight text-black p-3 font-bold rounded w-2/3 mx-auto my-0 text-center mt-20 hidden hover:resume md:visible" href={resume} download="joes-resume">Download Resume</a>
             {/* Contact Links */}
-            <ul className='footer-links w-full flex justify-evenly items-center'>
+            <ul className='footer-links hidden w-full md:flex justify-evenly items-center'>
                 {/* Linked In */}
                 <a className="transition-transform-colors hover:bg-li-blue hover:-translate-y-1 duration-500 rounded-full" href="https://www.linkedin.com/in/joerdixon/" target='_blank' rel="noreferrer" name="Link to Joe's LinkedIn"><img className="contactlink" src={lisvg} alt="LinkedIn logo" /></a>
                 {/* GitHub */}
--- src/components/Navbar/Navigation.jsx
+++ src/components/Navbar/Navigation.jsx
@@ -5,13 +5,15 @@ import aboutsvg from "../../assets/icons/about.svg"
 import skillsvg from "../../assets/icons/skills.svg"
 import projectsvg from "../../assets/icons/projects.svg"
 import contactsvg from "../../assets/icons/contact.svg"
+import hamburger from '../../assets/icons/hamburger.png'
 
 // TODO: Cool hover effects and active styles
 // TODO: Add hover effect for resume button
 
 function Nav(props) {
     return (
-        <div className='flex flex-col justify-between h-1/3' >
+       <>
+        <div className=' h-1/3 hidden md:flex flex-col justify-between' >
             <div className="navlink hover:navhover" onClick={() => { props.setView("about") }}>
                 <img src={aboutsvg} alt="about icon" />
                 <p>About Me</p>
@@ -29,6 +31,10 @@ function Nav(props) {
                 <p>Contact</p>
             </div>
         </div>
+        <div className="flex space-evenly md:hidden">
+            <img className="min-w-[40px]" src={hamburger} alt="Hamburger menu" />
+        </div>
+       </> 
     )
 }
 
