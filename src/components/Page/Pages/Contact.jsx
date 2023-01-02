6e63b1ef7881cf9f875f3214cae1ef25418b2fef
joerdixon <joseph35dixon@gmail.com>
2023-01-02 15:01:14 -0800

    contact revamp started

--- src/components/Page/Pages/Contact.jsx
+++ src/components/Page/Pages/Contact.jsx
@@ -37,16 +37,8 @@ function Contact() {
     }
 
     return (
-        <div className='footer-container w-5/6 h-2/3 my-0 mx-auto flex flex-col md:flex-row justify-evenly items-center'>
-            <h2 className='sectionheading'>Contact</h2>
-            <ul className='footer-links w-full flex justify-evenly items-center h-2/3'>
-                {/* Linked In */}
-                <a className="transition-transform-colors hover:bg-li-blue hover:-translate-y-1 duration-500 rounded-full" href="https://www.linkedin.com/in/joerdixon/" target='_blank' name="Link to Joe's LinkedIn"><img className="contactlink" src={lisvg} alt="LinkedIn logo" /></a>
-                {/* GitHub */}
-                <a className="transition-transform-colors hover:bg-gh-grey hover:-translate-y-1 duration-500 rounded-full" href="https://github.com/joerdixon" target='_blank' name="Link to Joe's GitHub Profile"><img className="contactlink" src={ghsvg} alt="GitHub logo" /></a>
-                {/* Gmail */}
-                <a className="transition-transform-colors hover:bg-gm-red hover:-translate-y-1 duration-500 rounded-full" href="mailto:joseph35dixon@gmail.com" target='_blank' name="Link to create and send an email."><img className="contactlink" src={gmsvg} alt="Mail logo." /></a>
-            </ul>
+        <div className='w-5/6'>
+            <h2 className='sectionheading md:sectionheading-md'>Contact</h2>
         </div>
         // <div className="contact-container">
         //     <h1>Contact me!</h1>
