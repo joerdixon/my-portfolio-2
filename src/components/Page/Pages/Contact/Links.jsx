61517920ddbb550922cf4d0065e77becfbdef931
joerdixon <joseph35dixon@gmail.com>
2023-01-03 12:19:07 -0800

    added more contact links

--- src/components/Page/Pages/Contact/Links.jsx
+++ src/components/Page/Pages/Contact/Links.jsx
@@ -1,16 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 // Logo SVG's
 import ghsvg from '../../../../assets/logos/github.svg'
 import lisvg from '../../../../assets/logos/linkedin.svg'
 import lcsvg from '../../../../assets/logos/leetcode.svg'
 import gmsvg from '../../../../assets/icons/mail.svg'
-import contact from '../../../../assets/icons/contact.svg'
 
 const links = [
     {
         name: "GitHub",
         url: "https://github.com/joerdixon",
         icon: ghsvg
+    },
+    {
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/in/joerdixon/",
+        icon: lisvg
+    },
+    {
+        name: "LeetCode",
+        url: "https://leetcode.com/joseph35dixon/",
+        icon: lcsvg
+    },
+    {
+        name: "E-mail",
+        url: "mailto:joseph35dixon@gmail.com",
+        icon: gmsvg
     },
 ]
 
