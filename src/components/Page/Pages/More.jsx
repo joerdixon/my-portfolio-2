9153682f3189d6eedf2438442803c841c252ea52
joerdixon <joseph35dixon@gmail.com>
2023-01-12 22:42:24 -0800

    more layout started

--- src/components/Page/Pages/More.jsx
+++ src/components/Page/Pages/More.jsx
@@ -17,20 +17,31 @@ import more from '../../../assets/icons/star.svg'
 
 function More() {
     return (
-        <div className='flex flex-col justify-top items-center md:w-auto'>
+        <div className='flex flex-col justify-top items-center w-full'>
             <div className='sectionheading md:sectionheading-md'>
                 <img src={more} alt="User icon on a book" className='hidden md:block md:heading-icon md:animate-slideX' />
                 <h2 className='md:animate-slideX2'>More</h2>
             </div>
-            <div className='flex flex-col justify-between items-center overflow-y-auto overflow-x-hidden'>
-                {/* TODO: Add highlighting and emphasis to certain words. */}
-                <div className='w-2/3 mt-20 mb-20 md:animate-slideX3'>
-                    <p className='text-left text-lg md:text-2xl w-full font-main'>Hello! My name is Joe and I am a Full Stack Web Developer who loves to create beautiful and functional websites. My love for the internet started with my first computer and only grew from that point onward. <br /><br />
-
-                        I’ve recently graduated from University of Washington’s Full Stack Bootcamp and have strong proficiency in both modern front-end and modern back-end technologies. Seeing the web grow and change over the years has inspired me to make my own contribution! I am passionate about creating amazing UX and accessible websites and hope to leave the internet more inclusive than I found it. <br /><br />
-
-                        Thanks for checking me out and I hope you enjoy the rest of my portfolio!
-                    </p>
+            <div className='w-full flex flex-col justify-evenly overflow-y-auto'>
+                <div className='more-section-l'>
+                    <h1 className='more-heading'>Heading</h1>
+                    <p>Content</p>
+                </div>
+                <div className='more-section-r'>
+                    <h1 className='more-heading'>Heading</h1>
+                    <p>Content</p>
+                </div>
+                <div className='more-section-l'>
+                    <h1 className='more-heading'>Heading</h1>
+                    <p>Content</p>
+                </div>
+                <div className='more-section-r'>
+                    <h1 className='more-heading'>Heading</h1>
+                    <p>Content</p>
+                </div>
+                <div className='more-section-l'>
+                    <h1 className='more-heading'>Heading</h1>
+                    <p>Content</p>
                 </div>
             </div>
         </div>
